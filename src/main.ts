e10647bd2358f21ccc2afff62ccde329623b7af4
leoTomio <leo__tomio@hotmail.com>
2023-06-26 10:36:08 -0300

    Criando Controller/Service/Modulo de Courses, apenas  para entender o processo de cada um

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  let port = "3005"
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`Api rodando na porta ${port} `)
 }
 bootstrap();
+
